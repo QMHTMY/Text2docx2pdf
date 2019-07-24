00a4641f1862097fafbfc90f917923cd460b1c4a
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-24 20:27:00 +0800

    modify file

--- program/Text2docx.py
+++ program/Text2docx.py
@@ -20,90 +20,65 @@
 import re, sys,time
 from os import listdir
 from docx import Document 
-from os.path import  exists basename 
+from os.path import exists, basename 
 
-#import sys
-#reload(sys)
-#sys.setdefaultencoding('utf-8')  #解决中文解析出错的问题,decode解决写入问题
+def getTitle(filename):
+    '''获取txt和title'''
+    try:
+        txtobj = open(filename)
+        title  = txtobj.readline() #.decode('utf-8')
+        text   = txtobj.read() #.decode('utf-8')     #decode还原为utf-8  encode
+        txtobj.close() 
+    except Exception as err:
+        print("Error, file does not exist")
+        txtobj.close() 
+        sys.exit(-1)
 
-def istxt(filename):
-	'''判断是否为txt文档'''
-	if filename.endswith('.txt'):
-		return True
-	else:
-		return False
-
-def getprefix1(filename):
-	'''提取电子书或任意文件的前缀名'''
-	name_pattern = re.compile(r'(.*?)\.')
-	try:
-		matched = name_pattern.search(filename)   #文件名，书名，不含点和后缀
-		prefix  = matched.group(1)
-	except Exception:                        
-		print("Name error")
-		sys.exit(-1)
-
-	return prefix                                   
-
-def getprefix(filename):
-    return filename.split('.')[0]
-
-def get_titl_text(filename):
-	'''获取txt和title'''
-	try:
-		txtobj = open(filename)
-		title  = txtobj.readline() #.decode('utf-8')
-		text   = txtobj.read() #.decode('utf-8')     #decode还原为utf-8  encode
-		txtobj.close() 
-	except Exception as err:
-		print("Error, file does not exist")
-		txtobj.close() 
-		sys.exit(-1)
-
-	return title, text
+    return title, text
 
 def write2docx(docxname,title,text):
-	'''将text内容写入docx'''
-	if not text:
-		print("Erro,no texture")
-		sys.exit(-1)
+    '''将text内容写入docx'''
+    if not text:
+        print("Erro,no texture")
+        sys.exit(-1)
 
-	if exists(docxname):
-		docx = Document(docxname)
-	else:
-		docx = Document()
+    if exists(docxname):
+        docx = Document(docxname)
+    else:
+        docx = Document()
 
-	try:
-		docx.add_heading(title)
-		docx.add_paragraph(text)
-	except Exception as err:
-		print("错误:%s"%err)
-		sys.exit(-1)
+    try:
+        docx.add_heading(title)
+        docx.add_paragraph(text)
+    except Exception as err:
+        print("错误:%s"%err)
+        sys.exit(-1)
 
-	docx.save(docxname)
+    docx.save(docxname)
+
+def transfer(fil):
+    docxname   = ''.join([fil.split('.')[0],'.docx'])
+    title,text = getTitle(fil)
+    write2docx(docxname,title,text)
 
 def text2docx():
-	'''主函数'''
-	argv = sys.argv
-	if len(argv) < 2:
-		prog_name = basename(argv[0])
-		print("Usage: %s [s1.txt] or %s -a"%(prog_name,prog_name))
-		sys.exit(-1)
+    '''主函数'''
+    argv = sys.argv
+    if len(argv) < 2:
+        program = basename(argv[0])
+        print("Usage: %s [s1.txt] or %s -a"%(program,program))
+        sys.exit(-1)
 
-	if '-a' == argv[1]:
-		for fil in listdir('.'):
-			if not istxt(fil):
-				continue
-			docxname = getprefix(fil) + '.docx'
-			title, text = get_titl_text(fil)
-			write2docx(docxname,title,text)
-	else:
-		for fil in argv[1:]:
-			if not istxt(fil):
-				continue
-			docxname = getprefix(fil) + '.docx'
-			title, text = get_titl_text(fil)
-			write2docx(docxname,title,text)
+    if '-a' == argv[1] or '--all' == argv[1]:
+        for fil in listdir('.'):
+            if not fil.endswith('.txt'):
+                continue
+            transfer(fil)
+    else:
+        for fil in argv[1:]:
+            if not fil.endswith('.txt'):
+                continue
+            transfer(fil)
 
 if __name__ == "__main__":
     start = time.time()
