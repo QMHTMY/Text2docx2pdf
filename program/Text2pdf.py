332159414f0cc3bb9b1d392e3be07e731f6d5385
Shieber <QMH_XB_FLTMY@yahoo.com>
2020-07-24 17:01:08 +0800

    modified files

--- program/Text2pdf.py
+++ program/Text2pdf.py
@@ -1,35 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#    Author: Shieber
-#    Date: 2019.07.24
-#
-#                             APACHE LICENSE
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-#
-#                            Function Description
-#    transfer txt file to pdf file.
-#    -a option means transfer all txt files in the current directory to pdf.
-#
-#    Copyright 2019 
-#    All Rights Reserved!
+# Author: Shieber
+# Date: 2019.07.24
+# Modified: 2020.07.24
 
-import sys,time
-from os.path import  basename 
+import sys
+import time
+from os.path import basename 
 from subprocess import call
-
-def transfer(fils):
-    for fl in fils:
-        if fl.endswith('.txt'):
-            call(f'Text2docx {fl} >/dev/null', shell=True)
-            call(f'Docx2pdf {''.join([fl.split('.')[0],'.docx'])}%s >/dev/null', shell=True)
 
 def trans2pdf():
     argv = sys.argv
@@ -43,7 +22,10 @@ def trans2pdf():
     else:
         fils = argv[1:]
 
-    transfer(fils)
+    for fl in fils:
+        if fl.endswith('.txt'):
+            call(f'Text2docx {fl} >/dev/null', shell=True)
+            call(f'Docx2pdf {''.join([fl.split('.')[0],'.docx'])}%s >/dev/null', shell=True)
 
 if __name__ == '__main__':
     start = time.time()
